7720fc601ca6f5e3a61b6d46d710dabcc1d25417
Sharaf <ahmedsharaf@shgardi.app>
2023-08-19 02:34:54 +0300

    Initial Commit

---
+++ MyZestyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyZestyTask
+//
+//  Created by Sharaf on 19/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
